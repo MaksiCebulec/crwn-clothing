b456f8668f4d5231d4f3f0c87634697b471fc624
Maksi <maksi.cebulec@gmail.com>
2022-11-22 20:20:01 +0100

    9th commit added logo and styling to nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-
 const Shop = () => {
   return (
     <h1>This is the shop page!</h1>
