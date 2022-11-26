8fe08f4c78c0e3e2ef3cde6900dd71e5539caedd
Maksi <maksi.cebulec@gmail.com>
2022-11-26 17:42:33 +0100

    12th commit setting up user document with firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
     signInWithPopup,
     GoogleAuthProvider
 } from 'firebase/auth';
+import {
+    getFirestore,
+    doc,
+    getDoc,//get doc data
+    setDoc//set doc data
+} from 'firebase/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyC0GiISWrbXA1573u2pAGXg4VOlNJ8I758",
     authDomain: "crwn-clothing-db-7cc3f.firebaseapp.com",
@@ -26,3 +32,15 @@ provider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
     signInWithPopup(auth, provider);
+
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot.exists());
+}
