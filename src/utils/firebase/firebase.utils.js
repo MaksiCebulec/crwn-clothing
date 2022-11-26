cb5892ed8f44826ce51267f299b711c1abc7f338
Maksi <maksi.cebulec@gmail.com>
2022-11-26 17:21:44 +0100

    11th commit added firebase google popup authentication

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from 'firebase/auth';
+const firebaseConfig = {
+    apiKey: "AIzaSyC0GiISWrbXA1573u2pAGXg4VOlNJ8I758",
+    authDomain: "crwn-clothing-db-7cc3f.firebaseapp.com",
+    projectId: "crwn-clothing-db-7cc3f",
+    storageBucket: "crwn-clothing-db-7cc3f.appspot.com",
+    messagingSenderId: "919623415715",
+    appId: "1:919623415715:web:b64db7efe06e3389d70c16"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+    signInWithPopup(auth, provider);
